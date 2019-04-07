0d333015b2b48e118908a51b099cf3fd4f6ea704
gulyasr01 <43796559+gulyasr01@users.noreply.github.com>
2019-04-07 15:45:11 +0200

    opan all files in the vocab and make list of them

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
 import pandas as pd
 import word as wd
+import os
+import numpy as np
 
-w1 = pd.read_csv('words1.csv', header=None)
+words_list = []
+files = os.listdir('vocab')
+for i in files:
+    wi = pd.read_csv('vocab/'+i, header=None)
+    for j in range(wi.shape[0]):
+        words_list.append(wd.Word(wi, j))
+
+
+w1 = pd.read_csv('vocab/words_19_03_10.csv', header=None)
 
 l1 = wd.Word(w1, 0)
 
-print(l1.mean2)
+print(words_list[11].mean2)
