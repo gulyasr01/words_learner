fa3e3ccda7007bc1f578786c0e4aa211487923a1
Gulyas Robert <gulyasr01@gmail.com>
2019-05-04 23:26:17 +0200

    new function added to create the vocabulary in pandas dataframe

--- word.py
+++ word.py
@@ -25,3 +25,25 @@ def fill_vocab():
         for j in range(wi.shape[0]):
             words_list.append(Word(wi, j))
     return words_list
+
+
+# make a padas dataframe from the xlsx-es
+def create_df():
+    files = os.listdir('words')
+    eng = []
+    hun = []
+    for i in files:
+        xls = pd.read_excel('words/' + i, header=None)
+        for j in range(xls.shape[0]):
+            eng.append(xls.iloc[j, 2])
+            hun_means = []
+            for k in xls.iloc[j, 3:]:
+                if pd.isnull(k):
+                    break
+                else:
+                    hun_means.append(k)
+            hun.append(hun_means)
+    df = pd.DataFrame(eng, columns=['eng'])
+    df['hun'] = hun
+    df['score'] = int(0)
+    return df
